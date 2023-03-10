faad775cdc700ab7c92c6499033bb37f76a7fdc8
ningxiao yang <a2135825@163.com>
2023-03-10 16:04:27 +0800

    使用swagger生成文档

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,21 @@ import { AppModule } from './app/app.module';
 import { TransformInterceptor } from './interceptor/transform/transform.interceptor';
 import { HttpExecptionFilter } from './filters/http-execption/http-execption.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformInterceptor()); // 全局使用拦截器
   app.useGlobalFilters(new HttpExecptionFilter()); // 全局使用筛选器
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('blog-serve')
+    .setDescription('接口文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger-doc', app, document); // swaggerUI localhost:3000/swagger-doc swaggerJSON localhost:3000/swagger-doc-json
 
   await app.listen(3000);
 }
--- src/modules/article/dto/id.dto.ts
+++ src/modules/article/dto/id.dto.ts
@@ -1,7 +1,13 @@
 // src/modules/article/dto/id.dto.ts
 import { IsNotEmpty, Matches } from 'class-validator';
 import { regPositive } from 'src/utils/regex.util';
+import { ApiProperty } from '@nestjs/swagger';
 export class IdDTO {
+  @ApiProperty({
+    // 为swagger配置接口说明
+    description: '文章id',
+    example: 1,
+  })
   @Matches(regPositive, { message: '请输入有效id' })
   @IsNotEmpty({ message: 'id不能为空' })
   readonly id: number;
